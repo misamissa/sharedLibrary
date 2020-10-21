6d20bb0062ffb03d13af62cc17b3e598db502ada
Julia Tsygankova <owlbird40@gmail.com>
2020-10-21 16:45:15 +0500

    initial commit

---
+++ src/components/TestComponent/TestComponent.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+class TestComponentComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		<div>test</div>
+	}
+}
+export AddSchoolRightBlockComponent;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index-bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          {
+            loader:'sass-loader',
+            options: {
+              sassOptions: {
+                sourceMap: false,
+                includePaths: ['../src/styles']
+              },
+            },
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
